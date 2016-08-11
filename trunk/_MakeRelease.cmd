fb9f9f91fc222957d9152592d989a4614b1c1bfe
Fly Man <fly.man.opensim@gmail.com>
2016-08-11 16:26:55 +0200

    Updating the build script to make releases possible

--- trunk/_MakeRelease.cmd
+++ trunk/_MakeRelease.cmd
@@ -1,12 +1,12 @@
 REM cd "C:\Documents and Settings\Alphons\Mijn documenten\Visual Studio 2005\Projects\LSLEditor\bin\Debug"
-cd "bin\Debug"
+cd "..\bin\Release"
 
-del d:\temp\LsLEditor.zip
-del d:\temp\LsLEditor.exe.bz2
-del d:\temp\LsLEditor.exe.gz
+del LsLEditor.zip
+del LsLEditor.exe.bz2
+del LsLEditor.exe.gz
 
-"c:\Program Files\7-Zip\7z" a -tzip d:\temp\LsLEditor.zip LsLEditor.exe
-"c:\Program Files\7-Zip\7z" a -tbzip2 d:\temp\LsLEditor.exe.bz2 LsLEditor.exe
-"c:\Program Files\7-Zip\7z" a -tgzip d:\temp\LsLEditor.exe.gz LsLEditor.exe
+"c:\Program Files\7-Zip\7z" a -tzip LsLEditor.zip LsLEditor.exe
+"c:\Program Files\7-Zip\7z" a -tbzip2 LsLEditor.exe.bz2 LsLEditor.exe
+"c:\Program Files\7-Zip\7z" a -tgzip LsLEditor.exe.gz LsLEditor.exe
 
 pause
